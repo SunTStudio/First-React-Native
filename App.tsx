f0c1070d3e74520464738b46068deb552572b33f
SunTStudio <mahsunmuh0@gmail.com>
2025-09-15 11:29:15 +0700

    first commit

--- App.tsx
+++ App.tsx
@@ -1,45 +1,72 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import { View, Text, Button, Image, TouchableOpacity, StyleSheet } from 'react-native';
+import React from 'react';
 
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
-import {
-  SafeAreaProvider,
-  useSafeAreaInsets,
-} from 'react-native-safe-area-context';
+function showAlert() {
+  alert('Button Clicked!');
+}
 
-function App() {
-  const isDarkMode = useColorScheme() === 'dark';
-
+export default function App() {
   return (
-    <SafeAreaProvider>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <AppContent />
-    </SafeAreaProvider>
+    <View style={style.area}>
+      <View style={style.KotakMerah}></View>
+      <View style={style.KotakKuning}></View>
+      <View style={style.KotakHijau}></View>
+     </View>
   );
 }
 
-function AppContent() {
-  const safeAreaInsets = useSafeAreaInsets();
 
-  return (
-    <View style={styles.container}>
-      <NewAppScreen
-        templateFileName="App.tsx"
-        safeAreaInsets={safeAreaInsets}
-      />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
+const style = StyleSheet.create({
+  area :{
+    flexDirection: 'row-reverse',
+    justifyContent: 'space-around',
+    marginTop: 50,
   },
-});
 
-export default App;
+  KotakMerah : {
+    width: 100, 
+    height: 100, 
+    backgroundColor: 'red',
+  
+  },
+
+  // KotakBiru : {
+  //   width: 100, 
+  //   height: 100, 
+  //   backgroundColor: 'blue',
+  //   marginTop: 20,
+  // },
+
+  KotakHijau : {
+    width: 100, 
+    height: 100, 
+    backgroundColor: 'green',
+  },
+
+  KotakKuning : {
+    width: 100, 
+    height: 100, 
+    backgroundColor: 'yellow',
+  },
+
+  // KotakUngu : {
+  //   width: 100, 
+  //   height: 100, 
+  //   backgroundColor: 'purple',
+  //   marginTop: 20,
+  // },
+
+  // KotakHitam : {
+  //   width: 100, 
+  //   height: 100, 
+  //   backgroundColor: 'black',
+  //   marginTop: 20,
+  // },
+
+  // KotakPutih : {
+  //   width: 100, 
+  //   height: 100, 
+  //   backgroundColor: 'white',
+  //   marginTop: 20,
+  // },
+})
